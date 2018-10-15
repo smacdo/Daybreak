8edae8e648323f5c6f7ccf5885d82dbed7a9d786
Scott MacDonald <scott@smacdo.com>
2018-10-14 22:06:38 -0700

    Adding unit tests

--- src/DaybreakCoreTests/Resources/ObjModelLoaderTests.cpp
+++ src/DaybreakCoreTests/Resources/ObjModelLoaderTests.cpp
@@ -6,6 +6,27 @@
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace Daybreak;
 
+namespace Microsoft::VisualStudio::CppUnitTestFramework
+{
+    std::wstring ToString(const obj_face_t& v)
+    {
+        std::wstringstream ss;
+        ss << L"{"
+            << " {" << v.position[0] << L", " << v.position[1] << L", " << v.position[2] << L"}, "
+            << " {" << v.uv[0]       << L", " << v.uv[1]       << L", " << v.uv[2]       << L"}, "
+            << " {" << v.normal[0]   << L", " << v.normal[1]   << L", " << v.normal[2]   << L"} "
+            << "}";
+        return ss.str();
+    }
+
+    std::wstring ToString(const obj_face_vertex_t& v)
+    {
+        std::wstringstream ss;
+        ss << L"{" << v.p << L", " << v.t << L", " << v.n << L"}";
+        return ss.str();
+    }
+}
+
 TEST_CLASS(ObjModelLoaderTests)
 {
 public:
@@ -95,5 +116,118 @@ public:
         Assert::ExpectException<std::runtime_error>([] { ObjModelParser p; p.parse("vn a 2 3"); });
         Assert::ExpectException<std::runtime_error>([] { ObjModelParser p; p.parse("vn 1 x 2"); });
         Assert::ExpectException<std::runtime_error>([] { ObjModelParser p; p.parse("vn 1 2 z"); });
+    }
+
+    TEST_METHOD(F_P_Only_Add_Face)
+    {
+        ObjModelParser parser;
+
+        const char * ObjData = "v 10 20 30\nv 11 21 31\nv 12 22 32\nf 1 2 3\nf 3 2 1";
+        auto model = parser.parse(ObjData);
+
+        Assert::AreEqual(2, (int)model->groups[0].faces.size());
+        Assert::AreEqual({ {1, 2, 3}, {0, 0, 0}, {0, 0, 0} }, model->groups[0].faces[0]);
+        Assert::AreEqual({ {3, 2, 1}, {0, 0, 0}, {0, 0, 0} }, model->groups[0].faces[1]);
+    }
+
+    TEST_METHOD(F_PT_Adds_Face)
+    {
+        ObjModelParser parser;
+
+        const char * ObjData =
+            "v 10 20 30\n vt 0.2 0.4\n vn 0.1 0.2 0.3\n"
+            "v 11 21 31\n vt 0.3 0.5\n vn 0.4 0.5 0.6\n"
+            "v 12 22 32\n vt 0.4 0.5\n vn 0.7 0.8 0.9\n"
+            "f 1/2 2/1 3/3\n f 3/3 2/1 1/2";
+
+        auto model = parser.parse(ObjData);
+
+        Assert::AreEqual(2, (int)model->groups[0].faces.size());
+        Assert::AreEqual({ {1, 2, 3}, {2, 1, 3}, {0, 0, 0} }, model->groups[0].faces[0]);
+        Assert::AreEqual({ {3, 2, 1}, {3, 1, 2}, {0, 0, 0} }, model->groups[0].faces[1]);
+    }
+
+    TEST_METHOD(F_PTN_Adds_Face)
+    {
+        ObjModelParser parser;
+
+        const char * ObjData =
+            "v 10 20 30\n vt 0.2 0.4\n vn 0.1 0.2 0.3\n"
+            "v 11 21 31\n vt 0.3 0.5\n vn 0.4 0.5 0.6\n"
+            "v 12 22 32\n vt 0.4 0.5\n vn 0.7 0.8 0.9\n"
+            "f 1/2/3 2/1/1 3/3/2\n f 3/3/1 2/1/3 1/2/2";
+
+        auto model = parser.parse(ObjData);
+
+        Assert::AreEqual(2, (int)model->groups[0].faces.size());
+        Assert::AreEqual({ {1, 2, 3}, {2, 1, 3}, {3, 1, 2} }, model->groups[0].faces[0]);
+        Assert::AreEqual({ {3, 2, 1}, {3, 1, 2}, {1, 3, 2} }, model->groups[0].faces[1]);
+    }
+
+    TEST_METHOD(F_PN_Adds_Face)
+    {
+        ObjModelParser parser;
+
+        const char * ObjData =
+            "v 10 20 30\n vt 0.2 0.4\n vn 0.1 0.2 0.3\n"
+            "v 11 21 31\n vt 0.3 0.5\n vn 0.4 0.5 0.6\n"
+            "v 12 22 32\n vt 0.4 0.5\n vn 0.7 0.8 0.9\n"
+            "f 1//3 2//1 3//2\n f 3//1 2//3 1//2";
+
+        auto model = parser.parse(ObjData);
+
+        Assert::AreEqual(2, (int)model->groups[0].faces.size());
+        Assert::AreEqual({ {1, 2, 3}, {0, 0, 0}, {3, 1, 2} }, model->groups[0].faces[0]);
+        Assert::AreEqual({ {3, 2, 1}, {0, 0, 0}, {1, 3, 2} }, model->groups[0].faces[1]);
+    }
+
+    TEST_METHOD(F_Adds_Faces_To_Current_Group)
+    {
+        ObjModelParser parser;
+
+        const char * ObjData =
+            "v 10 20 30\n vt 0.2 0.4\n vn 0.1 0.2 0.3\n"
+            "v 11 21 31\n vt 0.3 0.5\n vn 0.4 0.5 0.6\n"
+            "v 12 22 32\n vt 0.4 0.5\n vn 0.7 0.8 0.9\n"
+            "f 1//3 2//1 3//2\ng group\n f 3//1 2//3 1//2";
+
+        auto model = parser.parse(ObjData);
+
+        Assert::AreEqual(1, (int)model->groups[0].faces.size());
+        Assert::AreEqual({ {1, 2, 3}, {0, 0, 0}, {3, 1, 2} }, model->groups[0].faces[0]);
+
+        Assert::AreEqual(1, (int)model->groups[1].faces.size());
+        Assert::AreEqual({ {3, 2, 1}, {0, 0, 0}, {1, 3, 2} }, model->groups[1].faces[0]);
+    }
+
+    TEST_METHOD(G_Creates_A_New_Group)
+    {
+        ObjModelParser parser;
+
+        const char * ObjData =
+            "v 10 20 30\n vt 0.2 0.4\n vn 0.1 0.2 0.3\n"
+            "v 11 21 31\n vt 0.3 0.5\n vn 0.4 0.5 0.6\n"
+            "v 12 22 32\n vt 0.4 0.5\n vn 0.7 0.8 0.9\n"
+            "f 1//3 2//1 3//2\ng first\n f 3//1 2//3 1//2\ng second";
+
+        auto model = parser.parse(ObjData);
+
+        Assert::AreEqual(3, (int)model->groups.size());
+        Assert::AreEqual(ObjModelParser::DefaultGroupName, model->groups[0].name);
+        Assert::AreEqual(std::string("first"), model->groups[1].name);
+        Assert::AreEqual(std::string("second"), model->groups[2].name);
+    }
+
+    TEST_METHOD(MTLLIB_Adds_Material_Library)
+    {
+        ObjModelParser parser;
+
+        const char * ObjData = "mtllib foobar.mtl\nmtllib foo.mtl \n mtllib blah.mtl";
+        auto model = parser.parse(ObjData);
+       
+        Assert::AreEqual(3, (int)model->materialLibraries.size());
+        Assert::AreEqual("foobar.mtl", model->materialLibraries[0].c_str());
+        Assert::AreEqual("foo.mtl", model->materialLibraries[1].c_str());
+        Assert::AreEqual("blah.mtl", model->materialLibraries[2].c_str());
     }
 };
